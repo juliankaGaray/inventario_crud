6103c3b52a21611feec856def5b03b69447afd38
Julian Garay <julcamgar@gmail.com>
2024-12-07 21:23:15 -0500

    Script_db2

---
+++ InventarioITCEET/soporte_sistemas/inventario/soporte_db.sql
@@ -0,0 +1,253 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.22-MariaDB : Database - soporte_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`soporte_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `soporte_db`;
+
+/*Table structure for table `auth_group` */
+
+DROP TABLE IF EXISTS `auth_group`;
+
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_group` */
+
+/*Table structure for table `auth_group_permissions` */
+
+DROP TABLE IF EXISTS `auth_group_permissions`;
+
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_group_permissions` */
+
+/*Table structure for table `auth_permission` */
+
+DROP TABLE IF EXISTS `auth_permission`;
+
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_permission` */
+
+insert  into `auth_permission`(`id`,`name`,`content_type_id`,`codename`) values (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can view log entry',1,'view_logentry'),(5,'Can add permission',2,'add_permission'),(6,'Can change permission',2,'change_permission'),(7,'Can delete permission',2,'delete_permission'),(8,'Can view permission',2,'view_permission'),(9,'Can add group',3,'add_group'),(10,'Can change group',3,'change_group'),(11,'Can delete group',3,'delete_group'),(12,'Can view group',3,'view_group'),(13,'Can add user',4,'add_user'),(14,'Can change user',4,'change_user'),(15,'Can delete user',4,'delete_user'),(16,'Can view user',4,'view_user'),(17,'Can add content type',5,'add_contenttype'),(18,'Can change content type',5,'change_contenttype'),(19,'Can delete content type',5,'delete_contenttype'),(20,'Can view content type',5,'view_contenttype'),(21,'Can add session',6,'add_session'),(22,'Can change session',6,'change_session'),(23,'Can delete session',6,'delete_session'),(24,'Can view session',6,'view_session'),(25,'Can add equipo',7,'add_equipo'),(26,'Can change equipo',7,'change_equipo'),(27,'Can delete equipo',7,'delete_equipo'),(28,'Can view equipo',7,'view_equipo'),(29,'Can add periferico',8,'add_periferico'),(30,'Can change periferico',8,'change_periferico'),(31,'Can delete periferico',8,'delete_periferico'),(32,'Can view periferico',8,'view_periferico'),(33,'Can add historial mantenimiento',9,'add_historialmantenimiento'),(34,'Can change historial mantenimiento',9,'change_historialmantenimiento'),(35,'Can delete historial mantenimiento',9,'delete_historialmantenimiento'),(36,'Can view historial mantenimiento',9,'view_historialmantenimiento');
+
+/*Table structure for table `auth_user` */
+
+DROP TABLE IF EXISTS `auth_user`;
+
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_user` */
+
+/*Table structure for table `auth_user_groups` */
+
+DROP TABLE IF EXISTS `auth_user_groups`;
+
+CREATE TABLE `auth_user_groups` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
+  CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_user_groups` */
+
+/*Table structure for table `auth_user_user_permissions` */
+
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+
+CREATE TABLE `auth_user_user_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `auth_user_user_permissions` */
+
+/*Table structure for table `django_admin_log` */
+
+DROP TABLE IF EXISTS `django_admin_log`;
+
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext DEFAULT NULL,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL CHECK (`action_flag` >= 0),
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `django_admin_log` */
+
+/*Table structure for table `django_content_type` */
+
+DROP TABLE IF EXISTS `django_content_type`;
+
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `django_content_type` */
+
+insert  into `django_content_type`(`id`,`app_label`,`model`) values (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(7,'inventario','equipo'),(9,'inventario','historialmantenimiento'),(8,'inventario','periferico'),(6,'sessions','session');
+
+/*Table structure for table `django_migrations` */
+
+DROP TABLE IF EXISTS `django_migrations`;
+
+CREATE TABLE `django_migrations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `django_migrations` */
+
+insert  into `django_migrations`(`id`,`app`,`name`,`applied`) values (1,'contenttypes','0001_initial','2024-12-07 22:06:10.394717'),(2,'auth','0001_initial','2024-12-07 22:06:10.678984'),(3,'admin','0001_initial','2024-12-07 22:06:10.738248'),(4,'admin','0002_logentry_remove_auto_add','2024-12-07 22:06:10.745002'),(5,'admin','0003_logentry_add_action_flag_choices','2024-12-07 22:06:10.752138'),(6,'contenttypes','0002_remove_content_type_name','2024-12-07 22:06:10.789895'),(7,'auth','0002_alter_permission_name_max_length','2024-12-07 22:06:10.818736'),(8,'auth','0003_alter_user_email_max_length','2024-12-07 22:06:10.834091'),(9,'auth','0004_alter_user_username_opts','2024-12-07 22:06:10.844840'),(10,'auth','0005_alter_user_last_login_null','2024-12-07 22:06:10.866097'),(11,'auth','0006_require_contenttypes_0002','2024-12-07 22:06:10.868296'),(12,'auth','0007_alter_validators_add_error_messages','2024-12-07 22:06:10.876249'),(13,'auth','0008_alter_user_username_max_length','2024-12-07 22:06:10.890398'),(14,'auth','0009_alter_user_last_name_max_length','2024-12-07 22:06:10.903246'),(15,'auth','0010_alter_group_name_max_length','2024-12-07 22:06:10.918618'),(16,'auth','0011_update_proxy_permissions','2024-12-07 22:06:10.926478'),(17,'auth','0012_alter_user_first_name_max_length','2024-12-07 22:06:10.938069'),(18,'inventario','0001_initial','2024-12-07 22:06:11.018127'),(19,'sessions','0001_initial','2024-12-07 22:06:11.034321'),(20,'inventario','0002_auto_20241207_1843','2024-12-07 23:43:42.387991');
+
+/*Table structure for table `django_session` */
+
+DROP TABLE IF EXISTS `django_session`;
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `django_session` */
+
+/*Table structure for table `inventario_equipo` */
+
+DROP TABLE IF EXISTS `inventario_equipo`;
+
+CREATE TABLE `inventario_equipo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `numero_placa` varchar(50) NOT NULL,
+  `tipo_equipo` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `ubicacion` varchar(100) NOT NULL,
+  `fecha_ingreso` datetime(6) NOT NULL,
+  `fecha_salida` datetime(6) NOT NULL,
+  `disco_duro` varchar(100) DEFAULT NULL,
+  `nota` longtext DEFAULT NULL,
+  `sistema_operativo` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `numero_placa` (`numero_placa`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `inventario_equipo` */
+
+insert  into `inventario_equipo`(`id`,`numero_placa`,`tipo_equipo`,`marca`,`modelo`,`estado`,`ubicacion`,`fecha_ingreso`,`fecha_salida`,`disco_duro`,`nota`,`sistema_operativo`) values (2,'1025415','desk','Lenovo','9020','ok','bodega nueva','2024-12-08 01:27:10.143739','2024-12-08 01:27:10.143739','SSD','listo para entregar','win 11');
+
+/*Table structure for table `inventario_historialmantenimiento` */
+
+DROP TABLE IF EXISTS `inventario_historialmantenimiento`;
+
+CREATE TABLE `inventario_historialmantenimiento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `motivo` varchar(255) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `fecha` datetime(6) NOT NULL,
+  `equipo_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `inventario_historial_equipo_id_ad55fb0f_fk_inventari` (`equipo_id`),
+  CONSTRAINT `inventario_historial_equipo_id_ad55fb0f_fk_inventari` FOREIGN KEY (`equipo_id`) REFERENCES `inventario_equipo` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `inventario_historialmantenimiento` */
+
+/*Table structure for table `inventario_periferico` */
+
+DROP TABLE IF EXISTS `inventario_periferico`;
+
+CREATE TABLE `inventario_periferico` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `equipo_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `inventario_periferico_equipo_id_5b8fca50_fk_inventario_equipo_id` (`equipo_id`),
+  CONSTRAINT `inventario_periferico_equipo_id_5b8fca50_fk_inventario_equipo_id` FOREIGN KEY (`equipo_id`) REFERENCES `inventario_equipo` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `inventario_periferico` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
